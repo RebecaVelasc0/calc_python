3381ea3d8416f85fb4ee363662ff8d72dc8f0130
Rebeca <rebecavelasco2020@gmail.com>
2023-05-29 20:26:35 -0300

    calculadora

---
+++ calculadora/calc.py
@@ -0,0 +1,56 @@
+def soma():
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    print(num1+num2)
+    
+def div():
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    print(num1/num2)
+    
+def sub():
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    print(num1-num2)
+
+def mult():
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    print(num1*num2)
+
+
+operador = 1
+continuar = 1
+
+while operador:
+        print("Operadores da calculadora")
+        print("1 - Somar")
+        print("2 - Dividir")
+        print("3 - Subtrair")
+        print("4 - Multiplicar")
+
+        opcao = int(input("Digite a opção desejada: "))
+
+        if opcao == 1:
+            soma()
+        elif opcao == 2:
+            div()
+        elif opcao == 3:
+            sub()
+        elif opcao == 4:
+            mult()
+        else:
+            print("Digito inválido")
+
+  
+        
+
+
+
+
+
+
+
+
+
+        
