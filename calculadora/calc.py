63f1639ffbc37b229d40d451ad9af1177f0bad23
Rebeca <rebecavelasco2020@gmail.com>
2023-05-29 21:42:15 -0300

    atualização calculadora python

--- calculadora/calc.py
+++ calculadora/calc.py
@@ -18,6 +18,11 @@ def mult():
     num2 = float(input("Digite o segundo número: "))
     print(num1*num2)
 
+def expo():
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    print(num1**num2)
+
 
 operador = 1
 
@@ -27,6 +32,7 @@ while operador:
         print("2 - Dividir")
         print("3 - Subtrair")
         print("4 - Multiplicar")
+        print("5 - Exponenciação")
 
         opcao = int(input("Digite a opção desejada: "))
 
@@ -38,6 +44,8 @@ while operador:
             sub()
         elif opcao == 4:
             mult()
+        elif opcao == 5:
+            expo()
         else:
             print("Digito inválido")
 
